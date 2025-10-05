f349e041278c2457ecacd869c45bc68d07ea61b9
Virrael24 <t.zykova.msk@gmail.com>
2025-10-06 00:57:17 +0300

    час ночи...

---
+++ ludum-A2025/Assets/Assets/Scripts/Localizator/Data/TranslationEnum.cs
@@ -0,0 +1,14 @@
+﻿
+public enum TranslationEnum
+{
+   
+    PLAY = 1,
+    WELCOME = 2,
+    COLLECTOR = 3,
+    NOTE_1 = 4,
+    OBJECT_1 = 5,
+    OBJECT_2 = 6,
+    TITLE = 7,
+    NOTE_2 = 8,
+    OBJECT_3 = 9,
+}
